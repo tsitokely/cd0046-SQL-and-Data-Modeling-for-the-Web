e929217d76bf94b3e8b12439b356313eb323b74e
Tsitohaina Rakotondramasy <tsitohaina.toetra.rak@gmail.com>
2022-08-13 13:22:17 +0300

    Fixed venue view

--- app.py
+++ app.py
@@ -129,27 +129,7 @@ def index():
 def venues():
   # TODO: replace with real venues data.
   #       num_upcoming_shows should be aggregated based on number of upcoming shows per venue.
-  data=[{
-    "city": "San Francisco",
-    "state": "CA",
-    "venues": [{
-      "id": 1,
-      "name": "The Musical Hop",
-      "num_upcoming_shows": 0,
-    }, {
-      "id": 3,
-      "name": "Park Square Live Music & Coffee",
-      "num_upcoming_shows": 1,
-    }]
-  }, {
-    "city": "New York",
-    "state": "NY",
-    "venues": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }]
+  data=Venue.query.all()
   return render_template('pages/venues.html', areas=data);
 
 @app.route('/venues/search', methods=['POST'])
@@ -286,7 +266,6 @@ def delete_venue(venue_id):
 def artists():
   # ✔: replace with real data returned from querying the database
   data = Artist.query.all()
-
   return render_template('pages/artists.html', artists=data)
 
 @app.route('/artists/search', methods=['POST'])
